380046b1400d059eab52bb53e9843b1ebe28bbb6
abdulla sinan <123884526+sinaacb@users.noreply.github.com>
2024-08-13 08:00:27 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', 'Sinaaaan', 'TMS');
+$conn = mysqli_connect('localhost', 'root', 'password', 'TMS');
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
